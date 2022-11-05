5cf0397fa62b68844fb9916bbc891fda569c7da6
Harsh <itsharsh2001@gmail.com>
2022-11-05 16:28:21 +0530

    folder setup for createnft creators/creator done

---
+++ nft-marketplace/components/CreateNFT/CreateNft.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './CreateNft.module.css'
+
+const CreateNft = () => {
+  return (
+    <div>CreateNft</div>
+  )
+}
+
+export default CreateNft
---
+++ nft-marketplace/pages/creators/[id].js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Creator from '../../components/Creators/Creator'
+import Footer from '../../components/Home/Footer'
+import NavBar from '../../components/Home/NavBar'
+
+export default function CreatorPage () {
+  return (
+    <>
+        <NavBar/>
+        <Creator/>
+        <Footer/>
+    </>
+  )
+}
+
